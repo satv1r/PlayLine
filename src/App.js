0206e40863882bc152fbe11023fe672348afd28c
Satvir Sandhu <356Sandhu@gmail.com>
2020-11-01 17:32:03 -0500

    add assets and do some cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [playersData, setPlayersData] = useState([]);
+
+  const getPlayersData = async () => {
+    const data = await fetch(
+      "https://playline-dev-test.s3-us-west-2.amazonaws.com/playline-test.json"
+    );
+    setPlayersData(data);
+  };
+
+  useEffect(() => {
+    getPlayersData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Header 1</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia ea
+        dolore reiciendis provident, aspernatur esse et facilis nobis natus
+        praesentium repellat labore commodi? Voluptate, nisi possimus? Tenetur
+        enim corporis porro?
+      </p>
     </div>
   );
-}
+};
 
 export default App;
